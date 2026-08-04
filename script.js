4b6da244bc5d5050e930bf7043247d7bb945f90e
muhammadijlan34-prog <muhammadijlan34@gmail.com>
2026-08-05 00:17:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
- 
+ window.onload = function () {
+
+    setTimeout(function () {
+
+        document.getElementById("splash-screen").style.display = "none";
+
+        document.getElementById("main-content").style.display = "block";
+
+    }, 2000);
+
+};
