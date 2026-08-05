0a15652fb4f2d859e2cf88d16cb87662637720a7
muhammadijlan34-prog <muhammadijlan34@gmail.com>
2026-08-05 11:41:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ window.addEventListener("load",()=>{
     },2000);
 
 });
+/* Back To Top */
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>300){
+
+        topBtn.style.display="block";
+
+    }else{
+
+        topBtn.style.display="none";
+
+    }
+
+});
+
+topBtn.onclick=()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+};
