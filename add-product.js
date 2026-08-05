160bdde8e1747ff4d0f5cc9718fe0866fb6d5d0f
muhammadijlan34-prog <muhammadijlan34@gmail.com>
2026-08-05 17:41:55 +0530

    Update add-product.js

--- add-product.js
+++ add-product.js
@@ -1,1 +1,65 @@
- 
+// ==========================================
+// BABY KID - ADD PRODUCT
+// Part 11A
+// ==========================================
+
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Firebase Config
+
+const firebaseConfig = {
+
+    apiKey: "const firebaseConfig = {
+  apiKey: "AIzaSyC6nT8HiXDargNR02jkYXJVOFaeYsX1ats",
+  authDomain: "baby-kid-4107e.firebaseapp.com",
+  projectId: "baby-kid-4107e",
+  storageBucket: "baby-kid-4107e.firebasestorage.app",
+  messagingSenderId: "45906293564",
+  appId: "1:45906293564:web:f4b76b01899e736189a439",
+  measurementId: "G-DTFHKQ1M3S"
+};",
+
+    authDomain: "YOUR_AUTH_DOMAIN",
+
+    projectId: "YOUR_PROJECT_ID",
+
+    storageBucket: "YOUR_STORAGE_BUCKET",
+
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+
+    appId: "YOUR_APP_ID"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+// Cloudinary Details
+
+const CLOUD_NAME = "yfid2on4";
+
+const UPLOAD_PRESET = "babykid";
+
+// Form Elements
+
+const productName = document.getElementById("productName");
+
+const productPrice = document.getElementById("productPrice");
+
+const productCategory = document.getElementById("productCategory");
+
+const productDescription = document.getElementById("productDescription");
+
+const productImage = document.getElementById("productImage");
+
+const saveButton = document.getElementById("saveProduct");
